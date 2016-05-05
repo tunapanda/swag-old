6c1a86ac410ac4281bcfb407702283c15e59ab91
Mikael Lindqvist <li.mikael@gmail.com>
2016-05-05 16:52:00 +0300

    cleanup

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-	require_once __DIR__."/extern/minimvc/src/dispatcher/WebDispatcher.php";
-
-	$dispather=new WebDispatcher(__DIR__."/src/controllers");
-	$dispather->setDefaultController("main");
-
-	$dispather->dispatch();
--- xapiproxy/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-	require_once __DIR__."/../src/utils/PhpProxy.php";
-	require_once __DIR__."/../config.php";
-
-	$proxy=new PhpProxy();
-	$proxy->setRemoteSite($xapiEndpoint);
-	$proxy->dispatch();
